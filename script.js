024f370a48f34a8a5a5f4ef764c813e0dc59398a
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2020-12-26 23:46:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
 let hrPosition = (hr*360/12)+(min*(360/60)/12);
 let minPosition = (min*360/60)+(sec*(360/60)/60);
 let secPosition = sec*360/60;
 
 function runTheClock() {
-
+    
     hrPosition = hrPosition+(3/360);
     minPosition = minPosition+(6/60);
     secPosition = secPosition+6;
